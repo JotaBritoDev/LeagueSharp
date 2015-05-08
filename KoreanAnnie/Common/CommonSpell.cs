79f16b2bb6f10d13f3a84a984d7ffdbe59a9026d
Pastel com coca <pastelcomcocalol@gmail.com>
2015-05-08 17:28:18 -0300

    Preparing Common Spell classes

---
+++ KoreanAnnie/Common/CommonSpell.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+using LeagueSharp.Common;
+
+namespace KoreanAnnie
+{
+    class CommonSpell : Spell
+    {
+        public CommonSpell(SpellSlot slot, float range = 0, TargetSelector.DamageType damageType = TargetSelector.DamageType.Physical)
+            : base (slot, range, damageType)
+        {
+
+        }
+
+        public MenuItem UseOnComboMenu { get; set; }
+        public MenuItem UseOnHarasMenu { get; set; }
+        public MenuItem UseOnLaneClearMenu { get; set; }
+
+        public bool UseOnCombo
+        {
+            get { return UseOnComboMenu.GetValue<bool>(); }
+        }
+
+        public bool UseOnHaras
+        {
+            get { return UseOnHarasMenu.GetValue<bool>(); }
+        }
+
+        public bool UseOnLaneClear
+        {
+            get { return UseOnLaneClearMenu.GetValue<bool>(); }
+        }
+    }
+}
--- KoreanAnnie/Common/CommonSpells.cs
+++ KoreanAnnie/Common/CommonSpells.cs
@@ -3,18 +3,78 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 using LeagueSharp;
 using LeagueSharp.Common;
 
 namespace KoreanAnnie
 {
-    abstract class CommonSpells
+    class CommonSpells : IEnumerable
     {
-        Spell Q { get; set; }
-        Spell W { get; set; }
-        Spell E { get; set; }
-        Spell R { get; set; }
-        Spell RFlash { get; set; }
+        public List<CommonSpell> SpellList;
+
+        public IEnumerator GetEnumerator()
+        {
+            return SpellList.GetEnumerator();
+        }
+
+        public CommonSpells()
+        {
+            SpellList = new List<CommonSpell>();
+        }
+
+        public float MaxRangeCombo
+        {
+            get
+            {
+                float range = 0f;
+
+                CommonSpell ultimate = SpellList.Where(x => x.Slot == SpellSlot.R).First();
+                if (ultimate.IsReady())
+                {
+                    range = ultimate.Range;
+                }
+                else
+                {
+                    foreach (CommonSpell spell in SpellList)
+                    {
+                        if ((spell.IsReady()) && (spell.Range > range))
+                        {
+                            range = spell.IsReady() ? spell.Range : 0;
+                        }
+                    }
+                }
+
+                return range;
+            } //TODO
+        }
+
+        public float MaxRangeHaras
+        {
+            get { return 0; } //TODO
+        }
+
+        public float MaxComboDamage(Obj_AI_Hero target)
+        {
+            float damage = 0f;
+
+            foreach (CommonSpell spell in SpellList)
+            {
+                damage += spell.IsReady() ? spell.GetDamage(target) : 0;
+            }
+
+            return damage;
+        }
+
+        public void AddSpell(CommonSpell spell)
+        {
+            SpellList.Add(spell);
+        }
+
+        public void RemoveSpell(CommonSpell spell)
+        {
+            SpellList.Remove(spell);
+        }
     }
 
     struct FlashStruct
