3bf013b6ff321ddd4cccef2d140d35c87dc19ced
Pastel com coca <pastelcomcocalol@gmail.com>
2015-05-19 00:58:27 -0300

    Fixing cast on best target

--- IgniteSharp/CastOnBestTarget.cs
+++ IgniteSharp/CastOnBestTarget.cs
@@ -29,23 +29,30 @@ namespace IgniteSharp
                 && ignite.IsReady()
                 && ObjectManager.Player.CountEnemiesInRange(ignite.Range) > 0)
             {
+                Console.WriteLine("teste2");
                 Obj_AI_Hero target;
-
-                if (menu.Item("ignite#target").GetValue<Slider>().Value == 0)
+                Console.WriteLine("teste2.1");
+                if (menu.Item("ignite#target").GetValue<StringList>().SelectedIndex == 0)
                 {
+                    Console.WriteLine("teste3");
                     target =
                         HeroManager.Enemies.Where(hero => hero.IsValidTarget(ignite.Range))
                             .OrderByDescending(hero => hero.Health)
                             .First();
+                    Console.WriteLine("teste4");
                 }
                 else
                 {
+                    Console.WriteLine("teste5");
                     target = TargetSelector.GetTarget(ObjectManager.Player, ignite.Range, TargetSelector.DamageType.True);
+                    Console.WriteLine("teste6");
                 }
 
                 if (target != null)
                 {
+                    Console.WriteLine("teste7");
                     ignite.CastOnUnit(target);
+                    Console.WriteLine("teste8");
                 }
             }
         }
