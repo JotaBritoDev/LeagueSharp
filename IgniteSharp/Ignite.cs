103c8d3cfe9f6781911f9ef69243391e2b55e026
Pastel com coca <pastelcomcocalol@gmail.com>
2015-05-18 23:37:34 -0300

    version 1.0.0.0 - Initial release

---
+++ IgniteSharp/Ignite.cs
@@ -0,0 +1,71 @@
+﻿namespace IgniteSharp
+{
+    using System;
+    using System.Linq;
+
+    using LeagueSharp;
+    using LeagueSharp.Common;
+
+    internal class Ignite
+    {
+        private readonly Spell ignite;
+
+        private Menu menu;
+
+        public Ignite()
+        {
+            ignite = new Spell(ObjectManager.Player.GetSpellSlot("SummonerDot"), 600);
+            ignite.SetTargetted(0.1f, float.MaxValue);
+
+            LoadMenu();
+
+            //Spellbook.OnCastSpell += SmartIgnite;
+            Game.OnWndProc += GetIgniteKey;
+        }
+
+        private void GetIgniteKey(WndEventArgs args)
+        {
+            if (args.Msg == (uint)WindowsMessages.WM_KEYDOWN
+                && ObjectManager.Player.Spellbook.ActiveSpellSlot == ignite.Slot
+                && ignite.IsReady()
+                && ObjectManager.Player.CountEnemiesInRange(ignite.Range) > 0)
+            {
+                Obj_AI_Hero target;
+
+                if (menu.Item("ignite#target").GetValue<Slider>().Value == 0)
+                {
+                    target =
+                        HeroManager.Enemies.Where(hero => hero.IsValidTarget(ignite.Range))
+                            .OrderByDescending(hero => hero.Health)
+                            .First();
+                }
+                else
+                {
+                    target = TargetSelector.GetTarget(ObjectManager.Player, ignite.Range, TargetSelector.DamageType.True);
+                }
+
+                if (target != null)
+                {
+                    ignite.Cast(target);
+                }
+            }
+        }
+
+        private void LoadMenu()
+        {
+            menu = new Menu("Ignite#", "ignite", true);
+            menu.AddItem(
+                new MenuItem("ignite#target", "Cast on").SetValue(
+                    new StringList(new[] { "Lowest target", "Use target selector" })));
+            menu.AddToMainMenu();
+        }
+
+        private void SmartIgnite(Spellbook sender, SpellbookCastSpellEventArgs args)
+        {
+            if (sender.Owner.IsMe && args.Slot == ignite.Slot)
+            {
+                args.Process = false;
+            }
+        }
+    }
+}
---
+++ IgniteSharp/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace IgniteSharp
+{
+    using System;
+
+    using LeagueSharp;
+    using LeagueSharp.Common;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (ObjectManager.Player.GetSpellSlot("SummonerDot") != SpellSlot.Unknown)
+            {
+                CustomEvents.Game.OnGameLoad += eventArgs => { Ignite inite = new Ignite(); };
+            }
+        }
+    }
+}
