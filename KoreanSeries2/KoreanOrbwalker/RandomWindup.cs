f83f43928ada13eb69f1071dd9c259293b1f49ca
Pastel com coca <ermaojota@gmail.com>
2015-07-06 03:05:31 -0300

    Initial release - Don't flame >=(

---
+++ KoreanSeries2/KoreanOrbwalker/RandomWindup.cs
@@ -0,0 +1,29 @@
+﻿namespace KoreanOrbwalker
+{
+    using System;
+
+    internal static class RandomWindup
+    {
+        #region Public Methods and Operators
+
+        public static int Get(FeelingLike feelingLike)
+        {
+            var rnd = new Random();
+            switch (feelingLike)
+            {
+                case FeelingLike.Korean:
+                    return rnd.Next(10);
+                case FeelingLike.Platinum:
+                    return rnd.Next(100);
+                case FeelingLike.Bronze:
+                    return rnd.Next(350);
+                case FeelingLike.Dumbass:
+                    return rnd.Next(700);
+                default:
+                    return 0;
+            }
+        }
+
+        #endregion
+    }
+}
